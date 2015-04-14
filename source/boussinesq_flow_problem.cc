91efd767ed3bcb3415f965434c3160d8330473cb
Filippo Salmoiraghi <fsalmoir@sissa.it>
2015-04-14 10:03:04 +0200

    include cleaned

--- source/boussinesq_flow_problem.cc
+++ source/boussinesq_flow_problem.cc
@@ -1,63 +1,3 @@
-/*#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/logstream.h>
-#include <deal.II/base/function.h>
-#include <deal.II/base/utilities.h>
-#include <deal.II/base/conditional_ostream.h>
-#include <deal.II/base/work_stream.h>
-#include <deal.II/base/timer.h>
-#include <deal.II/base/parameter_handler.h>
-
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/solver_bicgstab.h>
-#include <deal.II/lac/solver_cg.h>
-#include <deal.II/lac/solver_gmres.h>
-#include <deal.II/lac/constraint_matrix.h>
-#include <deal.II/lac/block_sparsity_pattern.h>
-#include <deal.II/lac/trilinos_block_vector.h>
-#include <deal.II/lac/trilinos_sparse_matrix.h>
-#include <deal.II/lac/trilinos_block_sparse_matrix.h>
-#include <deal.II/lac/trilinos_precondition.h>
-#include <deal.II/lac/trilinos_solver.h>
-
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
-#include <deal.II/grid/filtered_iterator.h>
-#include <deal.II/grid/tria_boundary_lib.h>
-#include <deal.II/grid/grid_tools.h>
-#include <deal.II/grid/grid_refinement.h>
-
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/dofs/dof_renumbering.h>
-#include <deal.II/dofs/dof_accessor.h>
-#include <deal.II/dofs/dof_tools.h>
-
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/fe_dgq.h>
-#include <deal.II/fe/fe_dgp.h>
-#include <deal.II/fe/fe_system.h>
-#include <deal.II/fe/fe_values.h>
-#include <deal.II/fe/mapping_q.h>
-
-#include <deal.II/numerics/vector_tools.h>
-#include <deal.II/numerics/matrix_tools.h>
-#include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/error_estimator.h>
-#include <deal.II/numerics/solution_transfer.h>
-
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <limits>
-#include <locale>
-#include <string>
-
-#include <deal.II/distributed/solution_transfer.h>
-
-#include <deal.II/base/index_set.h>
-#include <deal.II/distributed/tria.h>
-#include <deal.II/distributed/grid_refinement.h>*/
 
 #include "boussinesq_flow_problem.h"
 
@@ -508,8 +448,8 @@ using namespace dealii;
 
     scratch.stokes_fe_values.reinit (cell);
 
-    std::vector<Vector<double> > rhs_values (n_q_points,
-                                             Vector<double>(dim+1));
+    //std::vector<Vector<double> > rhs_values (n_q_points,
+    //                                         Vector<double>(dim+1));
 
     //right_hand_side.vector_value_list (scratch.stokes_fe_values
     //                                  .get_quadrature_points(),
@@ -554,11 +494,12 @@ using namespace dealii;
                                scratch.stokes_fe_values.JxW(q);
            }*/
 
-        //std::vector<double> rhs_values ();
+//        std::vector<double> rhs_values ();
+        Vector<double> rhs_values;
 
-        /*right_hand_side.vector_value (scratch.stokes_fe_values
+        right_hand_side.vector_value (scratch.stokes_fe_values
                                       .quadrature_point(q),
-                                      rhs_values);*/
+                                      rhs_values);
 
          /*const Tensor<1,dim> aux_rhs;
 
