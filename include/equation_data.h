bf56c9edf03c1bebcb6da03f451872bb2ba4b4d2
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-05-01 22:57:41 +0200

    indentation

--- include/equation_data.h
+++ include/equation_data.h
@@ -5,8 +5,8 @@ using namespace dealii;
 
 namespace EquationData
 {
-	const double eta                   = 1;    		/* Pa s       */
-	const double density               = 3300;    	/* kg / m^3   */  
+	const double eta                   = 1;         /* Pa s       */
+	const double density               = 3300;      /* kg / m^3   */
 	const double nu                    = 1.0;
 }
 
