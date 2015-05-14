ebe4af30928d4c9aacb0e74d5a47002d2289c363
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-05-14 22:03:50 +0200

    cleaning solution.h

--- include/solution.h
+++ include/solution.h
@@ -15,15 +15,9 @@ public:
 
   virtual void vector_value ( const Point<dim>   &p,
                               Vector<double> &value) const;
-  // const unsigned int  component = 0) const;
+
   virtual void vector_gradient (  const Point<dim>   &p,
                                   std::vector<Tensor<1,dim>> &gradients) const;
-
-  /*virtual Tensor<1,dim> value (const Point<dim>   &p,
-                const unsigned int  component = 0) const;*/
-
-  // virtual Tensor<2,dim + 1> gradient (const Point<dim>   &p,
-  // const unsigned int  component = 0) const;
 };
 
 #endif
