e1842b654c8b697ccb7aa45e44b00af476e59c35
alberto sartori <alberto.sartori86@gmail.com>
2015-07-03 16:00:51 +0200

    added some tests

---
+++ tests/heat_equation_02.cc
@@ -0,0 +1,55 @@
+#include "n_fields_problem.h"
+#include "interfaces/heat_equation.h"
+#include "tests.h"
+
+typedef TrilinosWrappers::MPI::BlockVector VEC;
+
+
+template<int fdim, int fspacedim, int fn_components>
+void test(NFieldsProblem<fdim,fspacedim,fn_components> &pb)
+{
+  pb.make_grid_fe();
+  pb.setup_dofs();
+
+
+  VEC &d = pb.differential_components();
+	VEC sol = pb.solution;
+	VEC sol_dot = pb.solution_dot;
+	VEC residual = sol;
+
+  d.print(deallog.get_file_stream());
+
+	
+  deallog << "sol       ";
+	sol.print(deallog.get_file_stream());
+  deallog << "sol_dot   ";
+  sol_dot.print(deallog.get_file_stream());
+	pb.residual(0.0,sol,sol_dot,residual);
+  deallog << "residual  ";
+  residual.print(deallog.get_file_stream());
+
+}
+
+
+using namespace dealii;
+int main (int argc, char *argv[])
+{
+
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv,
+                                                      numbers::invalid_unsigned_int);
+
+  initlog();
+
+  const int dim = 2;
+	deallog << "##########################################"<<std::endl;
+	deallog << "           NO BC APPLIED!                 "<<std::endl;
+	deallog << "##########################################"<<std::endl;
+
+  HeatEquation<dim> energy;
+  NFieldsProblem<dim,dim,1> n_problem (energy);
+  ParameterAcceptor::initialize(SOURCE_DIR "/parameters/heat_equation_02.prm", "used_parameters.prm");
+
+  test(n_problem);
+
+  return 0;
+}
