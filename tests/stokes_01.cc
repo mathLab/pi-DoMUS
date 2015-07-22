d4da92446962092151e1b62cf736af20c5debb7e
alberto sartori <alberto.sartori86@gmail.com>
2015-07-22 06:31:32 +0200

    fixed test

--- tests/stokes_01.cc
+++ tests/stokes_01.cc
@@ -3,36 +3,38 @@
 #include "tests.h"
 
 template<int dim, int spacedim, int n_components>
-void test(NFieldsProblem<dim,spacedim,n_components> &pb) {
+void test(NFieldsProblem<dim,spacedim,n_components> &pb)
+{
   for (pb.current_cycle=0; pb.current_cycle<pb.n_cycles; ++pb.current_cycle)
-  {
-    if (pb.current_cycle == 0)
     {
-      pb.make_grid_fe();
-      pb.setup_dofs(true);
+      if (pb.current_cycle == 0)
+        {
+          pb.make_grid_fe();
+          pb.setup_dofs(true);
+        }
+      else
+        pb.refine_mesh();
+
+      pb.constraints.distribute(pb.solution);
+
+      auto rhs = pb.create_new_vector();
+      auto du = pb.create_new_vector();
+
+      pb.residual(0.0, pb.solution, pb.solution_dot, *rhs);
+      pb.setup_jacobian(0.0,pb.solution,pb.solution_dot, *rhs,1.0);
+      pb.solve_jacobian_system(0.0, pb.solution, pb.solution_dot, *rhs,
+                               1.0, *rhs, *du);
+
+      pb.set_constrained_dofs_to_zero(*du);
+
+      pb.solution += *du;
+
+      pb.distributed_solution = pb.solution;
+
+      pb.eh.error_from_exact(*pb.mapping, *pb.dof_handler, pb.distributed_solution, pb.exact_solution);
+
+      pb.output_step(0.0, pb.solution, pb.solution_dot, 0, 0.0);
     }
-    else
-      pb.refine_mesh();
-
-    pb.constraints.distribute(pb.solution);
-
-    auto rhs = pb.create_new_vector();
-    auto du = pb.create_new_vector();
-
-    pb.residual(0.0, pb.solution, pb.solution_dot, *rhs);
-    pb.solve_jacobian_system(0.0, pb.solution, pb.solution_dot, *rhs,
-                             1.0, *rhs, *du);
-
-    pb.set_constrained_dofs_to_zero(*du);
-
-    pb.solution += *du;
-
-    pb.distributed_solution = pb.solution;
-
-    pb.eh.error_from_exact(*pb.mapping, *pb.dof_handler, pb.distributed_solution, pb.exact_solution);
-
-    pb.output_step(0.0, pb.solution, pb.solution_dot, 0, 0.0);
-  }
 
   pb.eh.output_table(pb.pcout);
 
