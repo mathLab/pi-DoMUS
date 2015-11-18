f5136d64a23cf8fafeca7535a9e95c492be4bdef
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2015-11-18 11:55:37 +0100

    working compressible neo-hookean test (iterative and direct solvers)

---
+++ tests/compressible_neo_hookean_direct_01.cc
@@ -0,0 +1,30 @@
+#include "pidomus.h"
+#include "interfaces/conservative/compressible_neo_hookean_direct.h"
+#include "tests.h"
+
+using namespace dealii;
+int main (int argc, char *argv[])
+{
+
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv,
+                                                      numbers::invalid_unsigned_int);
+
+  initlog();
+  deallog.depth_file(1);
+
+  const int dim = 3;
+  const int spacedim = 3;
+
+  CompressibleNeoHookeanInterface<dim,spacedim> cnh_body;
+  piDoMUS<dim,spacedim,dim,LADealII> solver (cnh_body);
+  ParameterAcceptor::initialize(SOURCE_DIR "/parameters/compressible_neo_hookean_direct_01.prm", "used_parameters.prm");
+
+
+  solver.run ();
+
+  auto sol = solver.get_solution();
+  for (unsigned int i = 0; i<sol.size(); ++i)
+    deallog << sol[i] << std::endl;
+
+  return 0;
+}
