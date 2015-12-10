909a0ea0545a388e9ad94e974eb277e53a157397
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2015-12-10 13:26:14 +0100

    fixed incompressible neo-hookean 2 fields test (deallog and output)

--- tests/incompressible_neo_hookean_two_fields.cc
+++ tests/incompressible_neo_hookean_two_fields.cc
@@ -11,6 +11,8 @@ int main (int argc, char *argv[])
 
   initlog();
 
+  deallog.depth_file(1);
+
   const int dim = 3;
   const int spacedim = 3;
 
@@ -18,8 +20,11 @@ int main (int argc, char *argv[])
   piDoMUS<dim,spacedim,LADealII> n_problem ("",energy);
   ParameterAcceptor::initialize(SOURCE_DIR "/parameters/incompressible_neo_hookean_two_fields.prm", "used_parameters.prm");
 
-
   n_problem.run ();
+
+  auto sol = n_problem.get_solution();
+  for (unsigned int i = 0; i<sol.size(); ++i)
+    deallog << sol[i] << std::endl;
 
   return 0;
 }
