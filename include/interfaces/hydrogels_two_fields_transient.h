8769856e92c43dda5a3fca047bd200ef7dc99a89
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2016-02-01 09:52:51 +0100

    First working transient two-fields problem with direct solver.

--- include/interfaces/hydrogels_two_fields_transient.h
+++ include/interfaces/hydrogels_two_fields_transient.h
@@ -112,6 +112,7 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
   ResidualType rt = 0;
   this->reinit (rt, cell, fe_cache);
   auto &Fs_res = fe_cache.get_deformation_gradients("solution", "u", displacement, rt);
+  auto &Fs_dot = fe_cache.get_deformation_gradients("solution_dot", "u", displacement, rt);
   auto &grads_mu = fe_cache.get_gradients("solution", "gradmu", chempot, rt);
   auto &mus = fe_cache.get_values("solution", "mu", chempot, rt);
 
@@ -131,6 +132,7 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
       energies[0] += psi*JxW[q];
 
       auto &F_res = Fs_res[q];
+      auto &F_dot = Fs_dot[q];
       const Tensor<2,dim,ResidualType> F_star = J.val()*transpose(invert(F_res));
 
       auto &grad_mu = grads_mu[q];
@@ -140,10 +142,12 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
       for (unsigned int i=0; i<residuals[0].size(); ++i)
         {
           auto grad_v = fev[displacement].gradient(i,q);
-//          auto test_mu = fev[chempot].value(i,q);
+          auto test_mu = fev[chempot].value(i,q);
           auto grad_test_mu = fev[chempot].gradient(i,q);
-//          residuals[0][i] += (test_mu*inner(F_star,grad_v)/Omega-inner(h,grad_test_mu)*)JxW[q];
-          residuals[0][i] -= (mu*inner(F_star,grad_v)/Omega+inner(h,grad_test_mu))*JxW[q];
+
+          residuals[0][i] -= (mu*inner(F_star,grad_v)/Omega+
+                            -scalar_product(F_star,F_dot)*test_mu/Omega
+                            +h*grad_test_mu)*JxW[q];
         }
 
 //      if (!compute_only_system_terms)
