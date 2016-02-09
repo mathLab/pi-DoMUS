d4f0d3033b64d4dbf130d6d3142bf3c751b36158
alberto sartori <alberto.sartori86@gmail.com>
2016-02-09 15:23:17 +0100

    added cahn-hilliard equation

---
+++ include/interfaces/cahn-hilliard.h
@@ -0,0 +1,118 @@
+/*! \addtogroup equations
+ *  @{
+ */
+
+#ifndef _cahn_hilliard_h
+#define _cahn_hilliard_h
+
+#include "pde_system_interface.h"
+
+
+using deal2lkit::DOFUtilities::inner;
+
+
+template <int dim, typename LAC>
+class CahnHilliard : public PDESystemInterface<dim, dim, CahnHilliard<dim,LAC>, LAC>
+{
+public:
+
+  ~CahnHilliard() {};
+
+  CahnHilliard();
+
+  template <typename EnergyType, typename ResidualType>
+  void energies_and_residuals(const typename DoFHandler<dim>::active_cell_iterator &cell,
+                              FEValuesCache<dim> &scratch,
+                              std::vector<EnergyType> &energies,
+                              std::vector<std::vector<ResidualType> > &local_residuals,
+                              bool compute_only_system_terms) const;
+
+
+private:
+  const double lambda = 1.0e-2;
+
+};
+
+template <int dim, typename LAC>
+CahnHilliard<dim,LAC>::CahnHilliard() :
+  PDESystemInterface<dim,dim,CahnHilliard<dim,LAC>, LAC>(
+    "Cahn-Hilliard problem",
+    2,1,
+    "FESystem[FE_Q(1)-FE_Q(1)]",
+    "c,mu","1,0"
+  )
+{}
+
+template <int dim, typename LAC>
+template <typename EnergyType, typename ResidualType>
+void
+CahnHilliard<dim,LAC>::
+energies_and_residuals(const typename DoFHandler<dim>::active_cell_iterator &cell,
+                       FEValuesCache<dim> &fe_cache,
+                       std::vector<EnergyType> &,
+                       std::vector<std::vector<ResidualType> > &local_residuals,
+                       bool ) const
+{
+  ResidualType alpha = 0;
+  this->reinit(alpha, cell, fe_cache);
+
+  const FEValuesExtractors::Scalar concentration(0);
+  const FEValuesExtractors::Scalar aux(1);
+
+
+  auto &cs_dot = fe_cache.get_values("solution_dot", "c", concentration, alpha);
+  auto &cs = fe_cache.get_values("solution", "c", concentration, alpha);
+  auto &grad_cs = fe_cache.get_gradients("solution", "c", concentration, alpha);
+
+  auto &mus = fe_cache.get_values("solution", "mu", aux, alpha);
+  auto &grad_mus = fe_cache.get_gradients("solution", "mu", aux, alpha);
+
+
+  const unsigned int n_q_points = cs.size();
+
+  auto &JxW = fe_cache.get_JxW_values();
+  auto &fev = fe_cache.get_current_fe_values();
+
+  for (unsigned int q=0; q<n_q_points; ++q)
+    {
+      const ResidualType &c = cs[q];
+      const ResidualType &mu = mus[q];
+
+      const Tensor<1,dim,ResidualType> &grad_c = grad_cs[q];
+      const Tensor<1,dim,ResidualType> &grad_mu = grad_mus[q];
+
+
+      const ResidualType &c_dot = cs_dot[q];
+
+      // f = 100*c^2*(1-c)^2
+      // f_prime = df/dc
+      ResidualType f_prime = 200.*(c-1.)*(c-1.)*c + 200.*(c-1.)*c*c;
+
+      for (unsigned int i=0; i<local_residuals[0].size(); ++i)
+        {
+          auto test_c = fev[concentration].value(i,q);
+          auto grad_test_c = fev[concentration].gradient(i,q);
+
+          auto test_mu = fev[aux].value(i,q);
+          auto grad_test_mu = fev[aux].gradient(i,q);
+          local_residuals[0][i] += (
+                                     c_dot*test_c
+                                     +
+                                     inner(grad_mu,grad_test_c)
+                                     +
+                                     mu*test_mu
+                                     -
+                                     f_prime*test_mu
+                                     -
+                                     lambda*inner(grad_c,grad_test_mu)
+                                   )*JxW[q];
+        }
+
+
+    }
+
+}
+
+
+#endif
+/*! @} */
---
+++ tests/cahn-hilliard_01.cc
@@ -0,0 +1,29 @@
+#include "pidomus.h"
+#include "interfaces/cahn-hilliard.h"
+#include "tests.h"
+
+using namespace dealii;
+int main (int argc, char *argv[])
+{
+
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv,
+                                                      numbers::invalid_unsigned_int);
+
+  initlog();
+  deallog.depth_file(1);
+
+  const int dim = 2;
+
+  CahnHilliard<dim,LADealII> p;
+  piDoMUS<dim,dim,LADealII> solver ("pidomus",p);
+  ParameterAcceptor::initialize(SOURCE_DIR "/parameters/cahn_hilliard_01.prm", "used_parameters.prm");
+
+
+  solver.run ();
+
+  auto sol = solver.get_solution();
+  for (unsigned int i = 0; i<sol.size(); ++i)
+    deallog << sol[i] << std::endl;
+
+  return 0;
+}
