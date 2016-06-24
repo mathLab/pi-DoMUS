4a194213ee1e4e5fbcc7b91ebb671cf12482a0c0
gabriel-jan <jan.gabriel@uni-ulm.de>
2016-06-25 00:25:22 +0200

    heart_fe files added

---
+++ include/heart_fe.h
@@ -0,0 +1,33 @@
+#ifndef __heart_fe_h
+#define __heart_fe_h
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/dofs/dof_handler.h>
+
+using namespace dealii;
+
+template <int dim,int spacedim>
+class Heart
+{
+public:
+  Heart ();
+  Heart (bool, const int);
+  Point<spacedim> push_forward (const Point<dim>, const int) const;
+
+private:
+  void reinit_data ();
+  void setup_system ();
+  void run_side ();
+  void run_bottom ();
+
+  Triangulation<dim>		   		triangulation;
+  FESystem<dim> 			       	fe;
+  DoFHandler<dim>			      	dof_handler;
+  std::vector<Vector<double> >      solution;
+
+  bool side;
+};
+
+#endif
