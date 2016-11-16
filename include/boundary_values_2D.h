a6873e9218b90be5d632743b4bf5b45f660c10a6
gabriel-jan <jan.gabriel@uni-ulm.de>
2016-11-16 17:12:58 +0100

    2D heart slice

---
+++ include/boundary_values_2D.h
@@ -0,0 +1,58 @@
+#ifndef __boundary_values_h
+#define __boundary_values_h
+
+#include <deal.II/base/function.h>
+#include <deal.II/lac/vector.h>
+#include "heart_fe.h"
+
+using namespace dealii;
+
+template <int dim>
+class BoundaryValues : public Function<dim>
+{
+public:
+  BoundaryValues (int color, bool derivative=false) 
+    : 
+    Function<dim>(2*dim+1), 
+    color(color),
+    derivative(derivative)
+    {}
+  BoundaryValues (int color, 
+                  double timestep, 
+                  double dt, 
+                  bool side, 
+                  int degree, 
+                  bool derivative=false) 
+    : 
+    Function<dim>(2*dim+1),
+    color(color), 
+    timestep(timestep),
+    dt(dt),
+    heartstep(timestep/heartinterval),
+    derivative(derivative),
+    heart(side, degree)
+  {}
+
+  virtual double value (const Point<dim>   &p,
+                        const unsigned int  component = 0) const;
+  virtual void vector_value (const Point<dim> &p,
+                             Vector<double>   &value) const;
+private:
+  int color;
+  double timestep;
+  double dt;
+  double heartinterval = 0.005;
+  int heartstep;// = timestep / heartinterval;
+  bool derivative;
+  Heart<2,3> heart; 
+  void transform_to_polar_coord(const Point<3> &p, 
+                                double rot, 
+                                double &angle, 
+                                double &height) const;
+  void swap_coord(Point<3> &p) const;
+  void get_heartdelta (const Point<dim> &p, Vector<double> &value, int heartstep) const;
+  void get_values (const Point<dim> &p, Vector<double> &value) const;
+  void get_values_dt (const Point<dim> &p, Vector<double> &value) const;
+};
+
+#endif
--- source/boundary_values_2D.cc
+++ source/boundary_values_2D.cc
@@ -1,6 +1,5 @@
-#include "boundary_values.h"
+#include "boundary_values_2D.h"
 #include <cmath>
-#include <iostream>
 #define PI 3.14159265358979323846
 
 using namespace dealii;
@@ -14,15 +13,13 @@ BoundaryValues<dim>::value (const Point<dim>  &p,
   Assert (component < this->n_components,
           ExcIndexRange (component, 0, this->n_components));
 
-  Vector<double> values(3);
+  Vector<double> values(2);
   BoundaryValues<dim>::vector_value (p, values);
 
   if (component == 0)
       return values(0);
   if (component == 1)
       return values(1);
-  if (component == 2)
-      return values(2);
   return 0;
 }
 
@@ -35,7 +32,7 @@ BoundaryValues<dim>::vector_value (const Point<dim> &p,
         BoundaryValues<dim>::get_values_dt(p, values);
     else
     {
-        if (color == 2) {
+        if (color == 3) {
             get_heartdelta(p, values, heartstep);
         }
         else
@@ -76,6 +73,7 @@ BoundaryValues<dim>::swap_coord (Point<3> &p) const
 {
   // swap x and z coordinate
   double tmp = p(0);
+  //std::cout << "Hallo error1" << std::endl;
   p(0) = p(2);
   p(2) = tmp;
 }
@@ -86,48 +84,52 @@ BoundaryValues<dim>::get_heartdelta (const Point<dim> &p,
                                      Vector<double>   &values,
                                      int heartstep) const
 {
-  if (color == 2)         //////////////////////////////// top face 
+  //onst Point<3> p (point(1), 0, point(0)); //?
+  if (color == 3)         //////////////////////////////// top face 
   {
       // convert to polar coordinates and rotate 45 degrees
-      double phi, h, rot = PI/4;
-      transform_to_polar_coord(p, rot, phi, h);
+      double phi, h, rot = -PI/4;
+      Point<3> artificial_p (p(1), p(0), 0);
+      transform_to_polar_coord(artificial_p, rot, phi, h);
 
       // transform back to cartesian
       double x, y, r;
-      x = p(2);
-      y = p(1);
+      x = p(0);
+      y = 0;
 
       r = sqrt(x*x + y*y);
       x = r * cos(phi);
       y = r * sin(phi);
       
       // calc delta
-      values(0) = 0;
-      values(1) = y - p(1);
-      values(2) = x - p(2);
+      values(0) = y-p(0);
+      values(1) = 0;
   }
-  else if (color == 1)    //////////////////////////////// bottom face
+  else if (color == 2)    //////////////////////////////// bottom face
   {
       double x, y;
-      x = p(2);
-      y = p(1);
+      x = p(0);
+      y = p(0);
 
       Point<2> two_dim_pnt (x, y);
-      std::cout << "(" << x << ", " << y << ")" << std::endl;
+      //std::cout << "(" << x << ", " << y << ")" << std::endl;
       //get heart boundary point
       Point<3> heart_p = heart.push_forward (two_dim_pnt, heartstep);
       swap_coord(heart_p);
-
+      //std::cout << "Hallo error2" << std::endl;
       // calc delta
-      values(0) = heart_p(0) - p(0);
-      values(1) = heart_p(1) - p(1);
-      values(2) = heart_p(2) - p(2);
+      values(0) = heart_p(1) - p(0);
+      values(1) = heart_p(0) - p(1);
+      //std::cout << "Hallo error3" << std::endl;
+      //values(0) = 0;
+      //values(1) = 0;
   }
-  else if (color == 0)    //////////////////////////////// hull
+  else if (color == 0 || color == 1)    //////////////////////////////// hull
   {
       // convert to polar coordinates and rotate -45 degrees
-      double phi, h, rot = -PI/4;
-      transform_to_polar_coord(p, rot, phi, h);
+      double phi, h, rot = -PI/2;
+      Point<3> artificial_p (p(1), p(0), 0);
+      transform_to_polar_coord(artificial_p, rot, phi, h);
       Point<2> polar_pnt (phi, h);
       
       //get heart boundary point
@@ -135,9 +137,11 @@ BoundaryValues<dim>::get_heartdelta (const Point<dim> &p,
       swap_coord(heart_p);
 
       // calc delta
-      values(0) = heart_p(0) - p(0);
-      values(1) = heart_p(1) - p(1);
-      values(2) = heart_p(2) - p(2);
+      //values(0) = heart_p(0) - p(0);
+      //values(1) = heart_p(1) - p(1);
+      //values(2) = heart_p(2) - p(2);
+      values(0) = heart_p(1) - p(0);
+      values(1) = heart_p(0) - p(1);
   }
 }
 
@@ -165,7 +169,7 @@ BoundaryValues<dim>::get_values (const Point<dim> &p,
 
     values(0) = u_(0);
     values(1) = u_(1);
-    values(2) = u_(2);
+    //values(2) = u_(2);
 }
 
 
@@ -175,24 +179,26 @@ void
 BoundaryValues<dim>::get_values_dt (const Point<dim> &p,
                                     Vector<double>   &values) const
 {
-    Vector<double> u(dim);      // u_t
-    Vector<double> u_(dim);     // u_t+1
-    double substep = (fmod (timestep, heartinterval)/dt + 1)
-                     / (heartinterval / dt);
+    Vector<double> u_(dim);       // u_t-1
+    Vector<double> u(dim);        // u_t
+    Vector<double> delta_u(dim);  // u_t - u_t-1
+    //double substep = (fmod (timestep, heartinterval)/dt + 1)
+    //                 / (heartinterval / dt);
 
+    BoundaryValues<dim>::get_heartdelta(p, u_, (heartstep-1 < 0) ? 0 : heartstep-1);
     BoundaryValues<dim>::get_heartdelta(p, u, heartstep);
-    BoundaryValues<dim>::get_heartdelta(p, u_, (heartstep+1 > 99) ? 0 : heartstep+1);
 
-    // (u_t+1 - u_t) / h
-    u_ -= u;
-    u_ /= heartinterval;
+    // (u_t - u_t-1) / h
+    delta_u = u;
+    delta_u -= u_;
+    delta_u /= heartinterval;
     // scale u_
-    u_ *= substep;
+    delta_u /= (heartinterval/dt);
 
-    values(0) = u_(0);
-    values(1) = u_(1);
-    values(2) = u_(2);
+    values(0) = delta_u(0);
+    values(1) = delta_u(1);
+    //values(2) = u_(2);
 }
 
 // Explicit instantiations
-template class BoundaryValues<3>;
+template class BoundaryValues<2>;
